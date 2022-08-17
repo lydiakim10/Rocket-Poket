4f13bd4bd6bd342ab339ddd8b00f9ff869c0fac6
Crestatic <dtnguy86@gmail.com>
2022-08-17 16:12:14 -0700

    added a function to save to local storage for tcg page

--- js/TCGscript.js
+++ js/TCGscript.js
@@ -13,7 +13,7 @@ var findCard = async id => {
     
     var response = await fetch(url);
     var card = await response.json();
-    console.log(card);
+    // console.log(card);
     renderCard(card);
 }
 
@@ -24,21 +24,31 @@ function renderCard(card) {
     
     for (var i = 0; i < 50; i++) {
         var pokemonEl = document.createElement('div');
-        pokemonEl.classList.add('pokemon');
+        pokemonEl.classList.add('pokemonCard');
         const urlImage = card.data[i].images.small
-        console.log(urlImage);
-        
+        // console.log(urlImage);
+       
         var pokeInnerHTML = `
             <div class="pokeImage"><img src=${urlImage}/></div>
-            <button class="collected btn">Have
-            <button class="wishlist btn">Want     
+            <button class="collected btn">Have</button>
+            <button class="wishlist btn">Want</button>
         `;
-        console.log(pokeInnerHTML);
+        // console.log(pokeInnerHTML);
         pokemonEl.innerHTML = pokeInnerHTML;
-        console.log(pokemonEl);
+        // console.log(pokemonEl);
         containerEl.appendChild(pokemonEl);
-
     }
-    
 }
+
+
+var HaveBtnEl = $('.collected');
+
+HaveBtnEl.click(function(event) {
+    event.preventDefault();
+
+    var imgEl = $(this).siblings('src').val();
+    console.log(imgEl);
+    localStorage.setItem(Have, JSON.stringify(imgEl));
+})
+
 
