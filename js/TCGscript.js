b968f2d7932ad9abee99b70960ceedc55b7e7a9a
Crestatic <dtnguy86@gmail.com>
2022-08-21 18:25:56 -0700

    final edits.  CSS and JS cleaning.

--- js/TCGscript.js
+++ js/TCGscript.js
@@ -15,30 +15,28 @@ function renderCard(card) {
         var pokemonEl = document.createElement('div');
         pokemonEl.classList.add('pokemonCard');
         const urlImage = card.data[i].images.small
-        console.log(urlImage);
+        
        
         var pokeInnerHTML = `
             <div class="pokeImage"><img src=${urlImage}/></div>
             <button class="collected btn" id="haveButton${i}">Have</button>
             <button class="wishlist btn" id="wantButton${i}">Want</button>
         `;
-        console.log(pokeInnerHTML);
+        
         pokemonEl.innerHTML = pokeInnerHTML;
-        console.log(pokemonEl);
+        
         containerEl.appendChild(pokemonEl);
-        console.log(containerEl)
+        
     }
 }
 var HaveBtnEl = $('.collected');
 
 $(document).on('click', HaveBtnEl, function(event) {
-    console.log(event)
+    
     var imgEl = event.target.id
     var cardImage = document.getElementById(imgEl);
-    console.log(cardImage);
     var cardEl = cardImage.parentNode.children[0].children[0].getAttribute("src")
-    console.log(cardEl)
-    console.log(imgEl)
+   
     localStorage.setItem(imgEl, JSON.stringify(cardEl));    
     localStorage.setItem(imgEl, JSON.stringify(cardEl));
     }    
@@ -47,13 +45,11 @@ $(document).on('click', HaveBtnEl, function(event) {
 var CollectedBtnEl = $(".wishlist");
 
 $(document).on('click', CollectedBtnEl, function(event) {
-    console.log(event)
+    
     var imgEl = event.target.id
-    var cardImage = document.getElementById(imgEl);
-    console.log(cardImage);
+    var cardImage = document.getElementById(imgEl); 
     var cardEl = cardImage.parentNode.children[0].children[0].getAttribute("src")
-    console.log(cardEl)
-    console.log(imgEl)
+    
     localStorage.setItem(imgEl, JSON.stringify(cardEl));  
     }  
 )
